d3d7beef27a9f8885a73312bcdbbb63d37a091b2
Kuba Bielecki <kuba.bielecki@outlook.com>
2023-02-18 10:37:46 +0100

    Commit 4 added config.json file added temporary cipher.json file added _read_config method into Generator class modified method name form sub to at_bash

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import random
 import datetime
 import time
+import json
 from random_word import RandomWords
 
 
 class Generator:
 
+    def _read_config():
+        '''
+        The method is responsible for reading the kwords value from the json configuration file used to set the default k value in the generate_word_pssaword method
+        '''
+        f = open("config.json")
+        data = json.load(f)
+        output = data["generator"]["kwords"]
+
+        return output
+
     @staticmethod
-    def generate_word_password(k=3):
+    def generate_word_password(k=_read_config()):
         '''
         Method generates a random k-words "word password" using random_word lib
-        k parameter is default set on int value 3
         output_password is str var in which our password is creating
         i int var is counter in while loop
         '''
@@ -51,11 +61,10 @@ class Encrypter:
 
         for p in list_of_passwords:
             c_p = Encrypter.caesar(p)
-            s_p = Encrypter.substitution(c_p)
-            encrypted_passwords.append(s_p)
+            ab_p = Encrypter.at_bash(c_p)
+            encrypted_passwords.append(ab_p)
 
         return encrypted_passwords
-
 
     @classmethod
     def caesar(cls, phrase):
@@ -64,9 +73,9 @@ class Encrypter:
         '''
 
     @classmethod
-    def substitution(cls, phrase):
+    def at_bash(cls, phrase):
         '''
-        The method is responsible for invoking the substitution cipher for the given phrase parameter
+        The method is responsible for invoking the AtBash cipher for the given phrase parameter
         '''
 
 
